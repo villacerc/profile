35c3662c697aa05fb101ecd02d130ba71efa50ea
Carlo Villaceran <cvillaceran@gmail.com>
2019-04-27 22:40:22 -0700

    update footer links

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -9,13 +9,22 @@ const footer = () => {
       <div className="columns">
         <div className="column is-one-third is-offset-4">
           <div className={styles.socialButtons}>
-            <a className={classNames(styles.icon)} href="www.google.ca">
+            <a
+              className={classNames(styles.icon)}
+              href="https://github.com/villacerc"
+            >
               <i className="fab fa-github" />
             </a>
-            <a className={classNames(styles.icon)} href="www.google.ca">
+            <a
+              className={classNames(styles.icon)}
+              href="https://www.linkedin.com/in/carlovillaceran/"
+            >
               <i className="fab fa-linkedin-in" />
             </a>
-            <a className={classNames(styles.icon)} href="www.google.ca">
+            <a
+              className={classNames(styles.icon)}
+              href="https://angel.co/carlo-villaceran"
+            >
               <i className="fab fa-angellist" />
             </a>
           </div>
